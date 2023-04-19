113450f7915ada3e7a81e64ac3227e81ea9ed933
ptrv-dev <petrov99119@gmail.com>
2023-04-19 12:43:07 +0300

    useWindowSize hook optimization

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -2,18 +2,12 @@ import { useLayoutEffect, useState } from 'react';
 import { debounce } from '../utils/debounce';
 
 const useWindowSize = () => {
-  const [size, setSize] = useState([0, 0]);
+  const [size, setSize] = useState([window.innerWidth, window.innerHeight]);
   useLayoutEffect(() => {
     function updateSize() {
       setSize([window.innerWidth, window.innerHeight]);
     }
-    window.addEventListener(
-      'resize',
-      debounce(() => {
-        updateSize();
-      }, 250)
-    );
-    updateSize();
+    window.addEventListener('resize', debounce(updateSize, 200));
     return () => window.removeEventListener('resize', updateSize);
   }, []);
   return size;
