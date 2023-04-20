eb3022644de0240daab5e402e6cdcf32c2181889
ptrv-dev <petrov99119@gmail.com>
2023-04-20 11:46:07 +0300

    Products data filled

--- src/@types/custom.ts
+++ src/@types/custom.ts
@@ -5,6 +5,18 @@ export interface IBrand {
   image: string;
 }
 
+export interface IProduct {
+  _id: number;
+  brandId: number;
+  images: string[];
+  title: string;
+  description?: string;
+  sizes?: string[];
+  price: number;
+  rating: number;
+  createdAt: number;
+}
+
 export interface INews {
   _id: number;
   image: string;
